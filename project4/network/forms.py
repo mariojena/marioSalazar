a1d92b8e76827d491217cdd11d529609fb572d44
Mario Salazar <65868372+mariojena@users.noreply.github.com>
2023-02-19 13:49:08 -0500

    Delete project4 directory

--- project4/network/forms.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.forms import SetPasswordForm, UserChangeForm
-from .models import User
-
-class SetPasswordForm(SetPasswordForm):
-    class Meta:
-        model = User
-        fields = ['new_password1', 'new_password2']
